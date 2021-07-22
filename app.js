f05f7eb7605a3910238d8e68ea8eb0f282170f5a
MouryaManu <67451638+MouryaManu@users.noreply.github.com>
2021-07-22 19:39:23 +0530

    ahbrfwfn

--- app.js
+++ app.js
@@ -100,7 +100,6 @@ const scriptSrcUrls = [
     "https://kit.fontawesome.com/",
     "https://cdnjs.cloudflare.com/",
     "https://cdn.jsdelivr.net",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
 ];
 const styleSrcUrls = [
     "https://kit-free.fontawesome.com/",
@@ -109,6 +108,7 @@ const styleSrcUrls = [
     "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
